b49fd8cc336e5cc4448b104793f3ac5321fd954a
progdrum <brookforestscitech@gmail.com>
2019-12-30 22:18:03 -0700

    Committing some updates before change

--- viz-app/app/nhl_data_app.py
+++ viz-app/app/nhl_data_app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template, request
-from bokeh.embed import server_document
+from bokeh.embed import server_document, server_session
+from bokeh.client import pull_session
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = request.args.get('name')
-    if name == None:
-        name = 'Postulio'
-    return render_template('index.html', name=name)
+    return render_template('index.html')
 
 
 @app.route('/goalie-stats')
 def goalie_stats():
     stats_view = request.args.get('statsview', default='basic')
     if stats_view == 'basic':
-        script = server_document('https://localhost:5006/goalies_basic')
-        return render_template("goalies_basic.html", script=script)
+        with pull_session(url='http://localhost:5006/goalies_basic') as session:
+            script = server_session(session_id=session.id,
+                                    url='http://localhost:5006/goalies_basic')
+            return render_template("goalies_basic.html", script=script)
     elif stats_view == 'enhanced':
-        script = server_document('https://localhost:5006/goalies_faceted')
-        return render_template("goalies_advanced.html", script=script)
+        with pull_session(url='http://localhost:5007/goalies_faceted') as session:
+            script = server_session(session_id=session.id,
+                                    url='http://localhost:5007/goalies_faceted')
+            return render_template("goalies_advanced.html", script=script)
 
 
 if __name__ == '__main__':
